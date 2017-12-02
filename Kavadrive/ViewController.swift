d023db845348d88e1bbf1d8073c66f70a0c96c3f
Oleksii Tiurenkov <alterego4@gmail.com>
2017-12-02 20:51:57 +0200

    Initial Commit

---
+++ Kavadrive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kavadrive
+//
+//  Created by Aleksey Tyurenkov on 12/2/17.
+//  Copyright © 2017 com.ovt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
